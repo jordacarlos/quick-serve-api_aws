80528c8c5a619d24aa8c2ed78af2bd912d2b6534
Rick <ricklp@hotmail.com>
2024-07-17 08:08:12 -0300

    Bean Order and Product

--- src/main/java/br/com/fiap/techchallenge/quickserveapi/QuickServeApiApplication.java
+++ src/main/java/br/com/fiap/techchallenge/quickserveapi/QuickServeApiApplication.java
@@ -1,9 +1,15 @@
 package br.com.fiap.techchallenge.quickserveapi;
 
 import br.com.fiap.techchallenge.quickserveapi.Refatorar.controllers.CustomerRepositoryImpl;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.controllers.OrderRepositoryImpl;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.controllers.ProductRepositoryImpl;
 import br.com.fiap.techchallenge.quickserveapi.Refatorar.external.DatabaseConnection;
 import br.com.fiap.techchallenge.quickserveapi.Refatorar.interfaces.CustomerRepository;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.interfaces.OrderRepository;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.interfaces.ProductRepository;
 import br.com.fiap.techchallenge.quickserveapi.Refatorar.usecases.CustomerUseCases;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.usecases.OrderUseCases;
+import br.com.fiap.techchallenge.quickserveapi.Refatorar.usecases.ProductUseCases;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -30,5 +36,25 @@ public class QuickServeApiApplication {
 	@Bean
 	public CustomerUseCases customerUseCases(CustomerRepository customerRepository) {
 		return new CustomerUseCases(customerRepository);
+	}
+
+	@Bean
+	public OrderRepository OrderRepository(DatabaseConnection databaseConnection) {
+		return new OrderRepositoryImpl(databaseConnection);
+	}
+
+	@Bean
+	public OrderUseCases orderUseCases(OrderRepository orderRepository) {
+		return new OrderUseCases(orderRepository);
+	}
+
+	@Bean
+	public ProductRepository productRepository(DatabaseConnection databaseConnection) {
+		return new ProductRepositoryImpl(databaseConnection);
+	}
+
+	@Bean
+	public ProductUseCases ProductUseCases(ProductRepository productRepository) {
+		return new ProductUseCases(productRepository);
 	}
 }
